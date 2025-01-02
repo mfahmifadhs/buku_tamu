9a8da465db78bda27730b7dff2a8c4111ca23c78
mfahmifadhs <mfahmifadh2@gmail.com>
2025-01-02 14:43:23 +0700

    Tamu

--- resources/views/dashboard/pages/tamu/show.blade.php
+++ resources/views/dashboard/pages/tamu/show.blade.php
@@ -473,9 +473,9 @@
                                 <tr>
                                     <td>${item.no}</td>
                                     <td>
-                                        <a href="javascript:void(0);" class="show-modal" data-id="${item.id}">
-                                            <i class="fas fa-info-circle"></i>
-                                        </a>
+                                            <a href="javascript:void(0);" onclick="showModal(${item.id})">
+                                                <i class="fas fa-info-circle"></i>
+                                            </a>
                                         <a href="${editUrl}" id="edit-link-template">
                                             <i class="fas fa-edit"></i>
                                         </a>
@@ -527,46 +527,38 @@
                     // console.error('Error fetching data:', error);
                 }
             });
-        }
 
-        // Fungsi untuk menampilkan modal dengan data tamu
-        $(document).ready(function() {
-            $('body').on('click', '.show-modal', function() {
-                const idTamu = $(this).data('id');
+            // Fungsi untuk menampilkan modal dengan data tamu
+            window.showModal = function(idTamu) {
                 console.log(idTamu)
-                showModal(idTamu);
-            });
-        });
+                $.ajax({
+                    url: `{{ url('/tamu/detail/') }}/${idTamu}`,
+                    method: 'GET',
+                    dataType: 'json',
+                    success: function(data) {
+                        // Mengisi modal dengan data tamu
+                        $('#modal-id').text(data.id_tamu);
+                        $('#modal-novisit').text(data.nomor_visitor);
+                        $('#modal-nipnik').text(data.nik_nip);
+                        $('#modal-nama').text(data.nama_tamu);
+                        $('#modal-asal').text(data.area.nama_lantai + ', ' + data.area.nama_sub_bagian);
+                        $('#modal-notelp').text(data.no_telpon);
+                        $('#modal-alamat').text(data.alamat_tamu);
+
+                        $('#modal-tujuan').text(data.area.nama_lantai + ', ' + data.area.nama_sub_bagian);
+                        $('#modal-keperluan').text(data.keperluan);
+                        $('#modal-foto').attr('src', `{{ asset('storage/foto_tamu/') }}/${data.foto_tamu}`);
+
+                        // Menampilkan modal
+                        $('#detailModal').modal('show');
+                    },
+                    error: function(xhr, status, error) {
+                        // console.error('Error fetching detail:', error);
+                    }
+                });
+            };
+        }
     });
-
-    function showModal(idTamu) {
-        console.log(idTamu)
-        $.ajax({
-            url: `{{ url('/tamu/detail/') }}/${idTamu}`,
-            method: 'GET',
-            dataType: 'json',
-            success: function(data) {
-                // Mengisi modal dengan data tamu
-                $('#modal-id').text(data.id_tamu);
-                $('#modal-novisit').text(data.nomor_visitor);
-                $('#modal-nipnik').text(data.nik_nip);
-                $('#modal-nama').text(data.nama_tamu);
-                $('#modal-asal').text(data.area.nama_lantai + ', ' + data.area.nama_sub_bagian);
-                $('#modal-notelp').text(data.no_telpon);
-                $('#modal-alamat').text(data.alamat_tamu);
-
-                $('#modal-tujuan').text(data.area.nama_lantai + ', ' + data.area.nama_sub_bagian);
-                $('#modal-keperluan').text(data.keperluan);
-                $('#modal-foto').attr('src', `{{ asset('storage/foto_tamu/') }}/${data.foto_tamu}`);
-
-                // Menampilkan modal
-                $('#detailModal').modal('show');
-            },
-            error: function(xhr, status, error) {
-                // console.error('Error fetching detail:', error);
-            }
-        });
-    };
 </script>
 @endsection
 @endsection
